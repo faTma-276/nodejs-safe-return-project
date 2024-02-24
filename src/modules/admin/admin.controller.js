ddef16655cecf209fe67846a4955e1a75d9e5116
faTma-276 <159790973+faTma-276@users.noreply.github.com>
2024-02-24 03:05:19 +0200

    update

--- src/modules/admin/admin.controller.js
+++ src/modules/admin/admin.controller.js
@@ -24,7 +24,7 @@ export const signInAdmin = catchError(async (req,res,next)=>{
    let user = await adminModel.findOne({email})
    // console.log(user);
    if(!user || !bcrypt.compareSync(password , user.password))
-      return next (new AppError('incorrect email or password' , 409))
+      return next (new AppError('incorrect email or password' , 401))
       let token = jwt.sign({email:user.email,name:user.userName,id:user._id,role:user.role},process.env.jwt_KEY)
       res.status(201).json({message: 'success',user,token})
 
